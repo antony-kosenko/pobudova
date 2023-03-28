d5c30a41d4b9375c0e87a36767b9369ba17bef5e
DoOrQuit <kosankos@gmail.com>
2023-03-28 13:00:58 +0300

    base.html construct in progress

--- core/views.py
+++ core/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_view(request, *args, **kwargs):
-    return render(request, 'core/home.html', {})
+    return render(request, 'base.html', {})
 
-
-def test_view(request, *args, **kwargs):
-    return render(request, 'test.html', {})
 
